0a0de5676040936663156de8a324a014afd082e2
VergilSparda114514 <tawalish@163.com>
2026-06-25 01:00:00 +0800

    fixed build configurations

--- PTApp/Source/main.cpp
+++ PTApp/Source/main.cpp
@@ -1,21 +1,18 @@
 #include "RTXApp.h"
 
+int main()
+{
+	RTXApp app;
+	app.Run();
+}
+
 #ifdef _WIN64
 
 #include <Windows.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	RTXApp app;
-	app.Run();
-}
-
-#else
-
-int main(int argc, const char** argv)
-{
-	RTXApp app;
-	app.Run();
+	main();
 }
 
 #endif
