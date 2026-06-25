4cfd25d6467932534716eaa869639d399f1b0cb4
VergilSparda114514 <tawalish@163.com>
2026-06-25 17:24:41 +0800

    fixed compile compute shaders script

--- PTApp/Resource/compile_comp_shaders.cmd
+++ PTApp/Resource/compile_comp_shaders.cmd
@@ -1,9 +1,5 @@
 @echo off
 
-setlocal
-set SOURCE_FOLDER="./../Source/shaders/"
-set BINARIES_FOLDER="./shaders/"
-
-D:/VulkanSDK/1.4.321.1/Bin/glslc.exe %SOURCE_FOLDER%composite.comp -o %BINARIES_FOLDER%composite.spv
+%VULKAN_SDK%/Bin/glslc.exe ../Source/shaders/composite.comp -o ./shaders/composite.spv
 
 pause
